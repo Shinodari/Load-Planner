6d08e9bd35ac2ab42c3aaa8b7e2b843fdc97dece
hibino <zolo_atm@msn.com>
2023-08-21 21:29:40 +0700

    First Commit

--- src/Model/Size.java
+++ src/Model/Size.java
@@ -1,4 +1,27 @@
 package Model;
 
 public class Size {
+    private double length;
+    private double width;
+    private double height;
+
+    public Size(double s){
+        this.length = s;
+        this.width = s;
+        this.height = s;
+    }
+    public Size(double length,double width,double height){
+        this.length = length;
+        this.width = width;
+        this.height = height;
+    }
+    public double getLength(){
+        return length;
+    }
+    public double getWidth(){
+        return width;
+    }
+    public double getHeight(){
+        return height;
+    }
 }
