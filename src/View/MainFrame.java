38b8bd6b20e3d143bfa1a42b7db408bd43fc5dd1
Shinodari <shinodari_bb@hotmail.com>
2023-09-08 23:21:50 +0700

    feat(main frame):first mainframe

--- src/View/MainFrame.java
+++ src/View/MainFrame.java
@@ -1,15 +1,21 @@
 package View;
 
+import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.Frame;
+import java.awt.GridBagLayout;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
+import java.beans.PropertyVetoException;
 
+import javax.swing.Box;
 import javax.swing.JDesktopPane;
 import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 import javax.swing.SwingUtilities;
 
 public class MainFrame extends JFrame implements ActionListener {
@@ -22,11 +28,26 @@ public class MainFrame extends JFrame implements ActionListener {
 
 		//Setup MainMenuBar
 		JMenuBar mainMenuBar = new JMenuBar();
-		JMenu containerMenu = new JMenu("Container");
-		containerMenu.setActionCommand(containerMenu.getName());
-		containerMenu.addActionListener(this);
-		containerMenu.setMnemonic(KeyEvent.VK_C);
-		mainMenuBar.add(containerMenu);
+		
+		JMenu mainMenu = new JMenu("Main");
+		
+		JMenuItem containerMenuItem = new JMenuItem("Container");
+		containerMenuItem.setMaximumSize(new Dimension(200, 100));
+		containerMenuItem.setActionCommand(containerMenuItem.getName());
+		containerMenuItem.addActionListener(this);
+		
+		JMenuItem palletMenuItem = new JMenuItem("Pallet");
+		palletMenuItem.setMaximumSize(new Dimension(500, 100));
+		palletMenuItem.setActionCommand(palletMenuItem.getName());
+		palletMenuItem.addActionListener(this);
+		
+		JMenuItem shipmentMenuItem = new JMenuItem("Shipment");
+		
+		mainMenu.add(containerMenuItem);
+		mainMenu.add(palletMenuItem);
+		mainMenu.add(shipmentMenuItem);
+		
+		mainMenuBar.add(mainMenu);
 		
 		desktop = new JDesktopPane();
 		setContentPane(desktop);
@@ -41,7 +62,7 @@ public class MainFrame extends JFrame implements ActionListener {
 			@Override
 			public void run() {
 				MainFrame mainframe = new MainFrame();
-				mainframe.setMinimumSize(new Dimension(1200, 1000));
+				mainframe.setMinimumSize(new Dimension(1200, 750));
 				mainframe.setExtendedState(Frame.MAXIMIZED_BOTH);
 				mainframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 				mainframe.setVisible(true);
@@ -54,7 +75,18 @@ public class MainFrame extends JFrame implements ActionListener {
 	public void actionPerformed(ActionEvent e) {
 		switch(e.getActionCommand()) {
 		case "Container":
-			
+			/* -- This process in IssueNo: #2 (Assign: Shinodari) --
+			ContainerFrame containerFrame = new ContainerFrame();
+			containerFrame.setVisible(true);
+			desktop.add(containerFrame);
+			try {
+				containerFrame.setSelected(true);
+			} catch (PropertyVetoException e1) {
+				e1.printStackTrace();
+			}*/
+			break;
+		case "Pallet":
+			//ToDO for IssueNo: #3 (Assign: Hibino02)
 			break;
 		}
 	}
