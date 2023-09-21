f74d792ec549c1967e2e3fa70511e3712138d90d
Shinodari <shinodari_bb@hotmail.com>
2023-09-21 22:44:46 +0700

    feat(add container dialog):first

---
+++ src/View/AddContainerFrame.java
@@ -0,0 +1,13 @@
+package View;
+
+import java.awt.Container;
+
+public class AddContainerFrame extends AbstractContainerDialog {
+	private static final long serialVersionUID = 1L;
+
+	public AddContainerFrame(Container parent, String title) {
+		super(parent, title);
+		
+	}
+
+}
